55b63739207859c2893b25bfc69989b5b79a3690
Owen Ross <rossow@sheridancollege.ca>
2019-03-03 16:16:25 -0500

    Finished the person class. Main method is still not done.

---
+++ midterm review/GithubPractice/src/githubpractice/GithubPractice.java
@@ -0,0 +1,17 @@
+
+package githubpractice;
+
+/**
+ *
+ * @author oicr1
+ */
+public class GithubPractice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
